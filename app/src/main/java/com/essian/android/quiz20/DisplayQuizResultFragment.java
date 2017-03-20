7ac768af08561c6e4bf811d9ebd98d0ab72295b5
essian <jessicampdennis@hotmail.com>
2017-03-20 09:44:45 +0000

    anchors fab, updates activity titles, tidies strings and changes dialog button colours.

--- app/src/main/java/com/essian/android/quiz20/DisplayQuizResultFragment.java
+++ app/src/main/java/com/essian/android/quiz20/DisplayQuizResultFragment.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
@@ -46,11 +47,9 @@ public class DisplayQuizResultFragment extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final int score = getArguments().getInt(ARG_SCORE);
         final String name = getArguments().getString(ARG_NAME);
-        String message = "Well done " + name
-                + "! You scored " + score + " " + getResources().getQuantityString(R.plurals.point, score) +"."
-                + "\n\nDo you want to save your score or retry and change your answers?";
+        String message = getString(R.string.score_message, name, String.valueOf(score), getResources().getQuantityString(R.plurals.point, score));
         return new AlertDialog.Builder(getActivity())
-                .setTitle("Your score")
+                .setTitle(R.string.result_fragment_title)
                 .setMessage(message)
                 .setPositiveButton(R.string.save,
                         new DialogInterface.OnClickListener() {
@@ -59,15 +58,17 @@ public class DisplayQuizResultFragment extends DialogFragment {
                                 ScoreDbHelper scoreDbHelper = new ScoreDbHelper(getActivity());
                                 SQLiteDatabase db = scoreDbHelper.getReadableDatabase();
                                 scoreDbHelper.insertScore(db, name, score);
-//                                ContentValues values = new ContentValues();
-//                                values.put("NAME", name);
-//                                values.put("SCORE", score);
-//                                db.insert("SCORE", null, values);
                                 Intent intent = new Intent(getActivity(), MainActivity.class);
                                 startActivity(intent);
                             }
                         })
                 .setNegativeButton(R.string.retry, null)
                 .create();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        ((AlertDialog) getDialog()).getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(Color.GRAY);
     }
 }
